ff2706ff203bf340885e3d1671fb224f4f51c0ca
Md.Nazmul Hasan Hridoy <hridoy15-6450@s.diu.edu.bd>
2025-12-25 02:52:47 +0600

    react+vite, tailwind css , daisy ui , react router axios are install

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BiAngry } from "react-icons/bi";
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <BiAngry size={40} />
+      <button className='btn text-4xl'>hi</button>
+    </>
+  )
+}
+
+export default App
